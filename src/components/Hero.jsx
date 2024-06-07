07c81ecdcbd50a631178a4de831c2406ec611bf0
unknown <jhonerix23@gmail.com>
2024-06-08 00:40:42 +0800

    modified font size of subtitle in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ export default function Hero() {
         <h1 className='text-main-color dark:text-title-color font-extrabold text-4xl md:text-6xl lg:text-6xl'>
           Hi! I am <span className='text-accent-color'>Eric</span>
         </h1>
-        <h2 className='text-sub-title-color font-medium text-lg md:text-lg lg:text-xl'>Transforming Ideas into Interactive Reality</h2>
+        <h2 className='text-sub-title-color font-medium text-base md:text-lg lg:text-xl'>Transforming Ideas into Interactive Reality</h2>
         <p className='leading-7 md:leading-7 text-base md:text-base lg:text-base text-main-color dark:text-text-color'>
           <span className='text-accent-color font-medium'>A Student</span>,
           armed with the ability to create engaging websites. Infused with a passion for crafting immersive user experiences
